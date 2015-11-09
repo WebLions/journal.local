e77abfafe69146c73c6743b7f12f04c1c518c027
Igor Kobziev <igorok901@gmail.com>
2015-11-09 19:29:20 +0200

    add

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'angara5.beget.ru';
+$db['default']['username'] = 'fb3809sd_journal';
+$db['default']['password'] = 'journal_db';
+$db['default']['database'] = 'fb3809sd_journal';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
