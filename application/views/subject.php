b1a9ea9173eb9b9d45bfac2de770497c1e109c3c
ArgeSwarge <argecata@mail.ru>
2015-11-11 23:09:46 +0200

    subjects+users+techers

--- application/views/subject.php
+++ application/views/subject.php
@@ -1,120 +1,144 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Электронный журнал</title>
+
+    <title>Административная панель</title>
+
+    <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/prettyPhoto.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="css/plugins/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
-</head><!--/head-->
 
-   
-<div class="container">
-	<div class="row">
-		<div class="col-md-12 alert alert-success">
-		
-			<span>Header</span>
-			
-		</div>
-			
-	</div>
-	
-	<div class="row box first">  
-		<div class="col-md-8 content-border-right ">
-				
-				
-			<?php foreach($posts as $post):?>
-				<div class="row">
-					 <div class="col-md-12">		
-						<h3><?php echo $post['title'];?></h3>
-						<br>
-						<p><?php echo $post['description'];?></p>
-						<br>
-						<span class="date-float"><?php echo $post['date'];?></span>
-					</div>
-				</div>
-				<hr>
-				<?php endforeach;?>
-									
-				
-		
-		</div>
-		
-		<div class="col-md-4">
-		
-		
-			<div class="row">
-			
-				<div class="col-md-12">
-				
-				<form action="user/login" method="post">
-					<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1">!</span>
-						<input name="login" type="text" class="form-control" placeholder="Логин" aria-describedby="basic-addon1">
-					</div>
-					<br>
-					<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1">!</span>
-						<input name="password" type="text" class="form-control" placeholder="Пароль" aria-describedby="basic-addon1">
-					</div>
-					<br>
-					<button  style="float:right;" type="submit" class="btn btn-success">Войти</button>
-				
-				</div>
-			</form>
-			
-			</div>
-			<hr>
-			<div class="row">
-			
-				<div class="col-md-12">
-				
-				<div class="alert alert-success">
-				<span>Последнее обновление сайта:</span><br>
-				<span style="float:right;">09.11.2015</span><br></div>
-					
-				
-				</div>
-			
-			</div>
-		
-		</div>
-	
-	</div>
+</head>
 
+<body>
 
-			
-	<div class="row">
-        <div class="col-sm-12 alert alert-info">
-		
-            <span></span>
-        
-		</div>
-    </div>
-			
-</div><!--/.container-->
-    
+    <div id="wrapper">
 
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">Административная панель</a>
+            </div>
+            <!-- Top Menu Items -->
            
-     
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="/">Главная</a>
+                    </li>
+                    <li>
+                        <a href="#"Группы</a>
+                    </li>
+                    <li>
+                        <a href="#">Преподаватели</a>
+                    </li>
+                    <li>
+                        <a href="#" class = "active">Дисциплины</a>
+                    </li>
+                    <li>
+                        <a href="#">Пользователи</a>
+                    </li>
+					<li>
+                        <a href="#">Учебный план</a>
+                    </li>
+					<li>
+                        <a href="#">Сервисные операции</a>
+                    </li>
+                    <li>
+                        <a href="#">Выход</a>
+                    </li>
+                   
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
 
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Добавление пользователя
+                        </h1>
+											
+					</div>
+					
+					
+                </div>
+				
+				<div class="row">
+                    <form action="/admin/add_post" method="post">
+						<div class="col-md-12">
+							<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1">!</span>
+								<input name="title" type="text" class="form-control" placeholder="Заголовок" aria-describedby="basic-addon1">
+							</div>
+						<br>
+							<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1"></span>
+								<textarea name="description" type="text" class="form-control" placeholder="Текст новости" aria-describedby="basic-addon1"> </textarea>
+							</div>
+						<br>
+						
+						<button style="float:right;" type="submit" class="btn btn-success">Добавить пользователя</button>			
+						</div>
+				
+                </div>
+                <!-- /.row -->
+
+                
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
     <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.isotope.min.js"></script>
-    <script src="js/jquery.prettyPhoto.js"></script>
-    <script src="js/main.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="js/plugins/morris/raphael.min.js"></script>
+    <script src="js/plugins/morris/morris.min.js"></script>
+    <script src="js/plugins/morris/morris-data.js"></script>
+
 </body>
+
 </html>
