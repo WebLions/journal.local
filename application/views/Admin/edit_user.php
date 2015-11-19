86295679573826bb525a7fbf326474c3928abc53
ArgeSwarge <argecata@mail.ru>
2015-11-19 14:30:50 +0200

    user upd

--- application/views/Admin/edit_user.php
+++ application/views/Admin/edit_user.php
@@ -14,38 +14,52 @@
 						
   
 
-  <div class="tab-content">
-    <div id="groups" class="tab-pane fade in active">
+ <div class="tab-content">
+    <div id="user" class="tab-pane fade in active">
 		<div class = "row">
-			<div class = "col-md-12">
-						<h3>Список пользователей </h3>
+			<div class = "col-md-10">
+			<br>
+			<form action = "edit_user?id=<?=$user[0]['id']?>" method = "post">
+						<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1">Фамилия</span>
+								<input name="surname" type="text" value="<?=$user[0]['surname']?>" class="form-control"  aria-describedby="basic-addon1" required> 
+							</div>
 						<br>
-						<table width = "100%">
-							<tr>
-								<th style = "width: 50%">Пользователь ФИО</th>
-								<th style = "width: 50%">Логин</th>
-								<th style = "width: 20%;text-align:right">Функции</th>
-							</tr>
-                        
-						<?php foreach($users as $user):?>
-						
-							<tr>
-								
-									<td><?=$user['name']?></td>
-									<td><?=$user['login']?></td>
-									<td>
-										<a class="glyphicon glyphicon-trash btn btn-danger btn-xs" style="float:right;" href="admin/delete_user?id=<?=$user['id']?>"></a>
-										<a class="glyphicon glyphicon-pencil btn btn-success btn-xs" style="float:right;" href="admin/edit_user?id=<?=$user['id']?>"></a>
-				
-									</td>
-								
-							</tr>
-						
-						<?php endforeach; echo '<br>';?>
-                        
-				</table>
+						<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1">Имя</span>
+								<input name="name" type="text" value="<?=$user[0]['name']?>" class="form-control"  aria-describedby="basic-addon1" required> 
+							</div>
+						<br>
+						<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1">Отчество</span>
+								<input name="subname" type="text" value="<?=$user[0]['subname']?>" class="form-control"  aria-describedby="basic-addon1" required> 
+							</div>
+						<br>
+						<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1">Логин</span>
+								<input name="login" type="text" value="<?=$user[0]['login']?>" class="form-control"  aria-describedby="basic-addon1" required> 
+							</div>
+						<br>
+						<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1">Пароль</span>
+								<input name="password" type="password" class="form-control"  aria-describedby="basic-addon1"> 
+							</div>
+						<br>
+						<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1">Пароль ещё раз</span>
+								<input name="repassword" type="password" class="form-control"  aria-describedby="basic-addon1"> 
+							</div>
+							
+						<br>	
+							<select name="access">
+								<option selected value="1">Admin</option>
+								<option selected value="2">#</option>
+								<option selected value="3">@</option>
+							</select>						
+						<button style="float:right;" type="submit" class="btn btn-success">Сохранить</button>
+					</form>
 			</div>
-		</div>							
+		</div>						
 	</div>
   </div>
 		</div>
